0f35c80de73c4d035fdcbd609130825d6dcb0d4e
varunireddy <47571795+varunireddy@users.noreply.github.com>
2020-05-17 15:02:14 +0530

    Add files via upload

---
+++ feedback2/codes/ee18btech11005/spice/ee18btech11005_ngspice.py
@@ -0,0 +1,24 @@
+#Code by B.Varuni
+#date 17th,May 2020
+
+import numpy as np  
+from matplotlib import pyplot as plt  
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+data=np.loadtxt('ee18btech11005.dat')  #loading the data from the simulation output
+plt.plot(data[:,0],data[:,1])  
+plt.grid()
+plt.xlabel("time")
+plt.ylabel("unit step response")
+plt.title("Output from spice simulation")
+
+#if using termux
+#plt.savefig('./figs/ee18btech11005/ee18btech11005_spice.pdf')
+#plt.savefig('./figs/ee18btech11005/ee18btech11005_spice.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11005/ee18btech11005_spice.pdf"))
+#else
+#plt.show()
